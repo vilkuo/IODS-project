19946bfc89e9be20b947c0ccb451d83657cbf4d3
Ville Kuorikoski <ville.kuorikoski@uef.fi>
2022-12-04 13:51:08 +0200

    Updated the script to include the steps asked for in Week 5 assignment.

--- data/create_human.R
+++ data/create_human.R
@@ -3,6 +3,7 @@
 # This script modifies and combines two data sets into one, 'human'.
 # Data source: https://hdr.undp.org/
 
+# Week 4------------------------------------------------------------------------
 # Install packages
 library(tidyverse); library(dplyr)
 
@@ -47,7 +48,7 @@ gii <- gii %>% rename(
   GII = "Gender Inequality Index (GII)",
   Mat_Mor = "Maternal Mortality Ratio",
   Ado_Birth = "Adolescent Birth Rate",
-  Parli = "Percent Representation in Parliament",
+  Parli_F = "Percent Representation in Parliament",
   Edu_F = "Population with Secondary Education (Female)",
   Edu_M = "Population with Secondary Education (Male)",
   Labo_F = "Labour Force Participation Rate (Female)",
@@ -61,11 +62,38 @@ gii <- gii %>%
 # Combine the two data sets to a new data set called 'human'
 human <- inner_join(hd, gii, by = "Country")
 
+# Week 5------------------------------------------------------------------------
 # Check that the combined data set is correct
 dim(human) # The data set contains 195 observations and 19 variables
 str(human) # The data set contains all the variables in both input data sets and no duplicates
+# The data set describes certain aspects of human development in different countries. Variables 'Edu_FM' and 'Labo_FM' are the calculated ratios of
+# female to male populations with secondary education and labor force participation rates respectively. 'Parli_F' is the percent representation of females
+# in parlament. 'Mat_Mor' is the maternal mortality ratio and 'Abo_Birth' means the adolescent birth rate. 'GII' means the Gender Inequality Index and 
+# 'GNI' the Gross National Income per capita. 'Edu_Exp' is the expected years of education and 'Exp_Mean' is the mean years of education. 'Life_Exp' is the
+# life expectancy at birth and 'HDI' the Human Development Index.
 
+# Step 1. Mutate data
+str(human$GNI) # The GNI variable is already numeric so as.numeric() doesn't need to be used
 
+# Step 2. Keep certain columns
+human <- human[, c("Country", "Edu_FM", "Labo_FM", "Edu_Exp", "Life_Exp", "GNI", "Mat_Mor", "Ado_Birth", "Parli_F")]
+
+# Step 3. Remove missing values
+human_ <- filter(human, complete.cases(human)) %>% 
+  as.data.frame()
+
+# Step 4. Remove observations related to regions
+tail(human_$Country, 10) # Last 7 variables are regions and are therefore deleted
+last <- nrow(human_) - 7
+human_ <- human_[1:last, ]
+
+# Step 5. Move country names as a variable to the row names
+rownames(human_) <- human_$Country
+human_ <- select(human_, -Country)
+
+# Check the data set
+dim(human_) # The data set contains 155 observations and 8 variables as it should
+head(human_) # The rows are named with the Country names
 
 # Save the results and test the saved file--------------------------------------
 # Save the analysis data set to the current working directory, 'data'
